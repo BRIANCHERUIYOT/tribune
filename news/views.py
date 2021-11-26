c9b8ac6cc9d0ee71035712f8b621fc6fc7c27daa
BRIANCHERUIYOT <briancheruiyot@student.moringaschool.com>
2021-11-26 19:39:07 +0300

    adds the images object

--- news/views.py
+++ news/views.py
@@ -48,6 +48,6 @@ def search_results(request):
 def article(request,article_id):
     try:
         article = Article.objects.get(id = article_id)
-    except DoesNotExist:
+    except :
         raise Http404()
     return render(request,"all-news/article.html", {"article":article})
